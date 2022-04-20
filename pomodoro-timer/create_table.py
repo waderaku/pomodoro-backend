7a2b9a35f72f4c41952c8e2e1fae1a3f7b7ec38c
waderaku <wadebosh@icloud.com>
2022-04-20 12:57:42 +0000

    table作成コード一部修正

--- pomodoro-timer/create_table.py
+++ pomodoro-timer/create_table.py
@@ -1,19 +1,21 @@
 import os
+
 import boto3
 from dotenv import load_dotenv
 
-def create_db():
+
+def create_table():
     resource = boto3.resource(
         "dynamodb",
         endpoint_url=os.environ.get("DYNAMODB_ENDPOINT", None),
     )
     table_name = "pomodoro_info"
-    resource.create_table(
+    table = resource.create_table(
         TableName=table_name,
         AttributeDefinitions=[
             {"AttributeName": "ID", "AttributeType": "S"},
             {"AttributeName": "DataType", "AttributeType": "S"},
-            {"AttributeName": "DataValue", "AttributeType": "S"}
+            {"AttributeName": "DataValue", "AttributeType": "S"},
         ],
         KeySchema=[
             {"AttributeName": "ID", "KeyType": "HASH"},
@@ -21,18 +23,19 @@ def create_db():
         ],
         LocalSecondaryIndexes=[
             {
-                'IndexName': 'dataValueLSIndex',
-                'KeySchema': [
-                    {'AttributeName': 'ID','KeyType': 'HASH'},
-                    {'AttributeName': 'DataValue','KeyType': 'RANGE'}
+                "IndexName": "dataValueLSIndex",
+                "KeySchema": [
+                    {"AttributeName": "ID", "KeyType": "HASH"},
+                    {"AttributeName": "DataValue", "KeyType": "RANGE"},
                 ],
-                'Projection': {'ProjectionType': 'ALL'}
+                "Projection": {"ProjectionType": "ALL"},
             },
         ],
         ProvisionedThroughput={"ReadCapacityUnits": 1, "WriteCapacityUnits": 1},
     )
+    return table
 
 
 if __name__ == "__main__":
     load_dotenv("/root/workspaces/pomodoro-backend/pomodoro-timer/app/.env")
-    create_db()
+    create_table()
