efc15779ca61c6f77ed14bc2e69089281db234b3
waderaku <wadebosh@icloud.com>
2022-04-24 13:06:42 +0000

    #25 ユーザ更新テストコード実装

--- pomodoro-timer/app/presentation/controller/update_user_controller.py
+++ pomodoro-timer/app/presentation/controller/update_user_controller.py
@@ -1,6 +1,32 @@
-from app.presentation.http.common.user_model import UserModel
+from app.presentation.http.common.user_model import GoogleConfig, UserModel
+from app.usecase.service.update_user_service import update_user_service
 from fastapi import Header
 
 
 async def update_user(request: UserModel, userId: str = Header(None)):
-    pass
+
+    default_length = {
+        "work": request.defaultLength.work,
+        "rest": request.defaultLength.rest,
+    }
+    update_user_service(
+        user_id=userId,
+        is_google_linked=request.isGoogleLinked,
+        default_length=default_length,
+        google_config=_create_google_config(request.googleConfig),
+    )
+
+
+def _create_google_config(google_config: GoogleConfig | None):
+    if not google_config:
+        return
+    return {
+        "calendar": {
+            "id": google_config.calendar.id,
+            "name": google_config.calendar.name,
+        },
+        "task_list": {
+            "id": google_config.taskList.id,
+            "name": google_config.taskList.name,
+        },
+    }
---
+++ pomodoro-timer/app/usecase/service/update_user_service.py
@@ -0,0 +1,7 @@
+def update_user_service(
+    user_id: str,
+    is_google_linked: bool,
+    default_length: dict[str, int],
+    google_config: dict[str, dict] | None = None,
+):
+    pass
