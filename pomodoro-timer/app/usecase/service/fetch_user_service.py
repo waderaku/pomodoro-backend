01bfbbe394eb243431a48a2f0d39ed3b8181a075
waderaku <wadebosh@icloud.com>
2022-04-24 07:51:02 +0000

    #21 user取得コード実装

--- pomodoro-timer/app/usecase/service/fetch_user_service.py
+++ pomodoro-timer/app/usecase/service/fetch_user_service.py
@@ -1,13 +1,34 @@
+import os
 from dataclasses import dataclass
+
+import boto3
+from app.usecase.exception.custom_exception import NoExistUserException
+
+table_name = "pomodoro_info"
 
 
 @dataclass
 class User:
     user_id: str
     is_google_linked: bool
-    google_config: dict | None
     default_length: dict
+    google_config: dict | None
 
 
 def fetch_user_service(user_id: str) -> User:
-    pass
+    dynamodb = boto3.resource(
+        "dynamodb", endpoint_url=os.environ.get("DYNAMODB_ENDPOINT", None)
+    )
+    table = dynamodb.Table(table_name)
+    user = table.get_item(Key={"ID": user_id, "DataType": "user"}).get("Item", None)
+    if not user:
+        raise NoExistUserException()
+    user_info = user["UserInfo"]
+    return User(
+        user_id=user_id,
+        is_google_linked=user_info["is_google_linked"],
+        default_length=user_info["default_length"],
+        google_config=user_info["google_config"]
+        if user_info["is_google_linked"]
+        else None,
+    )
