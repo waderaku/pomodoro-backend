248b3beef010373f6a1ad111ab65a0a77e95a335
waderaku <wadebosh@icloud.com>
2022-04-24 06:38:55 +0000

    #18 docstringの追加

--- pomodoro-timer/app/usecase/service/register_user_service.py
+++ pomodoro-timer/app/usecase/service/register_user_service.py
@@ -7,6 +7,14 @@ table_name = "pomodoro_info"
 
 
 def register_user_service(user_id: str):
+    """新規にユーザーデータを登録する
+
+    Args:
+        user_id (str): ユーザーID
+
+    Raises:
+        AlreadyExistUserException: 既に対象のユーザーが存在する場合に発行される例外
+    """
     dynamodb = boto3.resource(
         "dynamodb", endpoint_url=os.environ.get("DYNAMODB_ENDPOINT", None)
     )
