e13de8e5d34e7ceb90f6a835a3aa0e7ce73c79da
waderaku <wadebosh@icloud.com>
2022-05-15 15:39:43 +0000

    close #31 task POST テストコード作成

---
+++ pomodoro-timer/app/usecase/service/register_task_service.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+def register_task_service(
+    user_id: str,
+    parent_id: str,
+    name: str,
+    estimatedWorkload: int,
+    deadline: datetime,
+    notes: str,
+):
+    pass
