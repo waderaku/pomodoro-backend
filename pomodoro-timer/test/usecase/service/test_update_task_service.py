a217791db765c16dba4fced1ca78a017a4898a50
waderaku <wadebosh@icloud.com>
2022-05-19 14:46:36 +0000

    #33 単純な更新のテスト時に全てのデータを確認するように変更

--- pomodoro-timer/test/usecase/service/test_update_task_service.py
+++ pomodoro-timer/test/usecase/service/test_update_task_service.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from decimal import Decimal
 from pathlib import Path
 from test.common import initial_process
-from test.db_util import fetch_deadline_task, fetch_task
+from test.db_util import fetch_deadline_task, fetch_task, fetch_task_by_task_id
 
 import pytest
 from app.usecase.service.update_task_service import update_task_service
@@ -27,8 +27,18 @@ with test_data_failed_path.open("r") as f:
     test_data_failed_list: list = json.load(f, parse_float=Decimal)
 
 ##########タスク更新正常系テスト##############
-@pytest.mark.parametrize("test_data_success", test_data_success_list[:3])
-def test_update_task_success(test_data_success: dict):
+def test_update_task_success():
+    request, answer = initial_process(test_data_success_list[0])
+    request["deadline"] = datetime.strptime(request["deadline"], "%Y-%m-%d")
+    update_task_service(**request)
+
+    task_list = fetch_task_by_task_id(request["user_id"], request["task_id"])
+
+    assert answer == task_list
+
+
+@pytest.mark.parametrize("test_data_success", test_data_success_list[1:3])
+def test_update_for_children_task_success(test_data_success: dict):
     request, answer = initial_process(test_data_success)
     request["deadline"] = datetime.strptime(request["deadline"], "%Y-%m-%d")
     update_task_service(**request)
